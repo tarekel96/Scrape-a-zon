62c74a33eb56621a712adf6ca6d60bd5f4ea28aa
Tarek <elhaj102@mail.chapman.edu>
2021-10-31 21:17:38 -0700

    added jsoup library to project and tested it to ensure it works

--- src/Controller/AppController.java
+++ src/Controller/AppController.java
@@ -10,6 +10,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * FXML Controller class
@@ -29,10 +33,24 @@ public class AppController {
     
     private String userInput;
     
+    public String testJsoup() {
+        String html = "<html><head><title>Sample Title</title></head>"
+                + "<body><p>Sample Content</p></body></html>";
+        String parsedText = "";
+        Document document = Jsoup.parse(html);
+        System.out.println(document.title());
+        Elements paragraphs = document.getElementsByTag("p");
+        for (Element paragraph : paragraphs) {
+            parsedText += paragraph.text();
+        }
+        return parsedText;
+    }
+    
     public void submit(ActionEvent event) {
         userInput = userSearchInput.getText();
         
-        outputLabel.setText(userInput);
+//        outputLabel.setText(userInput);
+        outputLabel.setText(testJsoup());
     }
     
     @FXML
---
+++ src/Controller/Scraper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author Tarek
+ */
+public class Scraper {
+    
+//    testJsoup()
+    
+}
